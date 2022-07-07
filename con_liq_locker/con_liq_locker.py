059f3a6773b7d83caac83246fe68ac2537b6f2da
Kelvin Takyi-Bobi <52892093+KELs7@users.noreply.github.com>
2022-07-07 18:27:28 +0000

    moved DEX import to the top

--- con_liq_locker/con_liq_locker.py
+++ con_liq_locker/con_liq_locker.py
@@ -1,4 +1,5 @@
-I = importlib
+import con_rocketswap_official_v1_1
+DEX = con_rocketswap_official_v1_1
 lock_info = Hash()
 lp_points = Hash(default_value=0)
 
@@ -8,8 +9,7 @@ def lock_lp(contract: str, amount: float, date: dict = None):
     assert amount > 0, "negative value not allowed!"
     user = ctx.caller
     if lp_points[contract, user] > 0:
-        dex = I.import_module('dex')
-        dex.transfer_liquidity_from(
+        DEX.transfer_liquidity_from(
             contract=contract, to=ctx.this, main_account=user, amount=amount)
         lock_info[contract, user]["amount"] += amount
         return lock_info[contract, user]
@@ -18,8 +18,7 @@ def lock_lp(contract: str, amount: float, date: dict = None):
         unlock_date = datetime.datetime(
             year=date["year"], month=date["month"], day=date["day"], hour=date["hour"], minute=date["minute"])
         assert unlock_date > now, "unlock_date must be set ahead from now!"
-        dex = I.import_module('dex')
-        dex.transfer_liquidity_from(
+        DEX.transfer_liquidity_from(
             contract=contract, to=ctx.this, main_account=user, amount=amount)
         lp_points[contract, user] += amount
         lock_info[contract, user] = {
@@ -51,8 +50,7 @@ def burn_lp(contract: str):
     lp_amount = lp_points[contract, user]
     assert lp_amount > 0, "no LPs to burn."
     lock_data = lock_info[contract, user]
-    dex = I.import_module('dex')
-    dex.transfer_liquidity(contract=contract, to="burn", amount=lp_amount)
+    DEX.transfer_liquidity(contract=contract, to="burn", amount=lp_amount)
     lp_points[contract, user] -= lp_amount
     lock_info[contract, user]["amount"] -= lp_amount
     return lock_info[contract, user]
@@ -66,8 +64,7 @@ def withdraw(contract: str):
     assert lp_amount > 0, "no LPs to withdraw."
     lock_data = lock_info[contract, user]
     assert now >= lock_data["unlock_date"], "cannot withdraw before unlock date."
-    dex = I.import_module('dex')
-    dex.transfer_liquidity(contract=contract, to=user, amount=lp_amount)
+    DEX.transfer_liquidity(contract=contract, to=user, amount=lp_amount)
     lock_info[contract, user]["amount"] -= lp_amount
     return lock_info[contract, user]
 
@@ -81,8 +78,7 @@ def withdraw_part(contract: str, amount: float):
     assert lp_amount > 0, "no LPs to withdraw"
     lock_data = lock_info[contract, user]
     assert now >= lock_data["unlock_date"], "cannot withdraw before unlock date."
-    dex = I.import_module('dex')
-    dex.transfer_liquidity(contract=contract, to=user, amount=amount)
+    DEX.transfer_liquidity(contract=contract, to=user, amount=amount)
     lp_points[contract, user] -= amount
     lock_info[contract, user]["amount"] -= amount
     return lock_info[contract, user]
