3a54d1b3dfb030c2ee55b52dc7b6c2fe6b1190a5
Kelvin Takyi-Bobi <52892093+KELs7@users.noreply.github.com>
2022-07-08 01:27:26 +0000

    replaced dates with datetime type
    dict solution is replaced + individual datetime args passing also replaced

--- con_liq_locker/con_liq_locker.py
+++ con_liq_locker/con_liq_locker.py
@@ -5,7 +5,7 @@ lp_points = Hash(default_value=0)
 
 
 @export
-def lock_lp(contract: str, amount: float, date: dict = None):
+def lock_lp(contract: str, amount: float, date: datetime.datetime = None):
     assert amount > 0, "negative value not allowed!"
     user = ctx.caller
     if lp_points[contract, user] > 0:
@@ -16,8 +16,8 @@ def lock_lp(contract: str, amount: float, date: dict = None):
         return lock_info[contract, user]
     else:
         assert date, "set a lock date!"
-        unlock_date = datetime.datetime(
-            year=date["year"], month=date["month"], day=date["day"], hour=date["hour"], minute=date["minute"])
+        assert isinstance(date, datetime.datetime), 'date is not a datetime type!'
+        unlock_date = date
         assert unlock_date > now, "unlock_date must be set ahead from now!"
         DEX.transfer_liquidity_from(
             contract=contract, to=ctx.this, main_account=user, amount=amount)
@@ -31,14 +31,15 @@ def lock_lp(contract: str, amount: float, date: dict = None):
 
 
 @export
-def extend_lock(contract: str, year: int, month: int, day: int, hour: int = 0, minute: int = 0):
+def extend_lock(contract: str, date: datetime.datetime):
     '''extend locking period whilst your LPs remain ontouched'''
     user = ctx.caller
+    assert date, "set a lock date!"
+    assert isinstance(date, datetime.datetime), 'date is not a datetime type!'
     assert lp_points[contract, user] > 0, "no locked LPs found."
     lock_data = lock_info[contract, user]
     unlock_date = lock_data["unlock_date"]
-    extended_date = datetime.datetime(
-        year=year, month=month, day=day, hour=hour, minute=minute)
+    extended_date = date
     assert extended_date > unlock_date, "extended date cannot be earlier or previous unlock date."
     lock_info[contract, user]["unlock_date"] = extended_date
     lock_info[contract, user] = lock_info[contract, user]
